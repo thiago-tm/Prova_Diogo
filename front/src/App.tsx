ae73f94d04b1cc9a67f316fc92dced5cf7b225c4
herosamx <bruno.somera@gmail.com>
2024-11-25 21:44:15 -0300

    Alteracao

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ConsultarCEP from "./components/pages/exemplos/Consultar";
-import ComponenteExemplo from "./components/pages/exemplos/ComponenteExemplo";
-import ProdutoListar from "./components/pages/produto/ProdutoListar";
-import ProdutoCadastrar from "./components/pages/produto/ProdutoCadastrar";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import ConsultarCEP from "";
+import ComponenteExemplo from "";
+import ProdutoListar from "";
+import ProdutoCadastrar from "";
+import { BrowserRouter, Link, Route, Routes } from "";
 
 function App() {
   return (
@@ -15,26 +15,18 @@ function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/pages/produto/listar">Listar Produtos</Link>
+              <Link to="/pages/tarefa/listar">Listar tarefa</Link>
             </li>
             <li>
-              <Link to="/pages/produto/cadastrar">Cadastrar Produto</Link>
+              <Link to="/pages/tarefa/cadastrar">Cadastrar tarefa</Link>
             </li>
             <li>
-              <Link to="/pages/endereco/consultar">Consultar CEP</Link>
+              <Link to="/pages/tarefa/consultar">Consultar tarefa</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<ProdutoListar />} />
-          <Route path="/pages/produto/listar" element={<ProdutoListar />} />
-          <Route
-            path="/pages/produto/cadastrar"
-            element={<ProdutoCadastrar />}
-          />
-          <Route path="/pages/endereco/consultar" element={<ConsultarCEP />} />
-          {/* Para páginas não encontradas */}
-          {/* <Route path="*" element={<Componente da Página não encontrada />} /> */}
+        
         </Routes>
       </BrowserRouter>
     </div>
